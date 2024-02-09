f546d89b294dae42e83de94eeba9e87d30c07d39
Konstantin Krasser <konstantin.krasser@student.tugraz.at>
2024-02-09 05:52:42 +0100

    ✅ Live-Server npm ≠ pnpm

--- nvim/lua/user/live-server.lua
+++ nvim/lua/user/live-server.lua
@@ -1,11 +1,11 @@
 local M = {
-  'barrett-ruth/live-server.nvim',
-  build = 'pnpm add -g live-server',
-  cmd = { 'LiveServerStart', 'LiveServerStop' },
-  config = true
+        'barrett-ruth/live-server.nvim',
+        build = 'npm add -g live-server',
+        cmd = { 'LiveServerStart', 'LiveServerStop' },
+        config = true
 }
-
-function M.config() 
+function M.config()
+    require('live-server').setup(opts)
 end
-  
+
 return M
